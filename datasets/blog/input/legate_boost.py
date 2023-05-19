4bb0923f10f2545cf8116584af9e814dd892e5c0
Bojan Tunguz <tunguz@gmail.com>
2023-05-19 12:12:59 -0400

    blog legate boost simple script

---
+++ datasets/blog/input/legate_boost.py
@@ -0,0 +1,5 @@
+import cunumeric as cn
+import legateboost as lbst
+
+X = cn.random.random((1000, 10))
+y = cn.random.random(X.shape[0])
---
+++ datasets/blog/scripts/legate_boost.py
@@ -0,0 +1,9 @@
+import cunumeric as cn
+import numpy as np
+import legateboost as lbst
+
+X_np = np.load('../input/X.npy')
+#Y_np = np.random.random('../input/Y.npy')
+
+X = cn.asarray(X_np)
+#Y = cn.asarray(Y_np)
