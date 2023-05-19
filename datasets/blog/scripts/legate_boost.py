1945218287416a5c20280173a7ecb79c232ea267
Bojan Tunguz <tunguz@gmail.com>
2023-05-19 12:51:00 -0400

    blog dataset legate boost test

--- datasets/blog/scripts/legate_boost.py
+++ datasets/blog/scripts/legate_boost.py
@@ -3,7 +3,12 @@ import numpy as np
 import legateboost as lbst
 
 X_np = np.load('../input/X.npy')
-#Y_np = np.random.random('../input/Y.npy')
+Y_np = np.load('../input/Y.npy')
 
-X = cn.asarray(X_np)
-#Y = cn.asarray(Y_np)
+X = cn.array(X_np)
+Y = cn.array(Y_np)
+
+print(X.shape)
+print(Y.shape)
+
+model = lbst.LBRegressor(verbose=1, random_state=0, max_depth=2).fit(X,Y)
